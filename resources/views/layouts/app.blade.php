37f8d2ea4de780d0ff6c979314fc124bc4ca9c1b
Gezod <refanggalintar@gmail.com>
2025-06-30 13:08:30 +0700

    update fav ico

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Absensi</title>
+    <title>BSIS Absensi</title>
+
+    {{-- Favicon --}}
+    <link rel="icon" type="image/png" href="{{ asset('images/Logo-Bank-Sampah-Surabaya-bank-sampah-induk-surabaya-v2 (1).png') }}">
 
     {{-- Bootstrap CSS --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -38,7 +41,7 @@
         @yield('content')
     </div>
 
-    {{-- Bootstrap JS (opsional jika pakai dropdown, sidebar collapse, dll) --}}
+    {{-- Bootstrap JS --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
