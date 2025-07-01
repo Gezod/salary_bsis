dd32be8749067207fe2cb6e13e4c6da9271b513c
Gezod <refanggalintar@gmail.com>
2025-07-01 08:29:54 +0700

    update ico

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }} - Login</title>
+     <title>BSIS Absensi</title>
+      <!-- Favicon -->
+    <link rel="icon" type="image/png" href="{{ asset('images\Logo-Bank-Sampah-Surabaya-bank-sampah-induk-surabaya-v2 (1).png') }}">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
